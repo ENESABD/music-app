509b19a3bee596e841e7f6edf8ce822dac7e729f
Ismail <ileghari1@gmail.com>
2021-04-17 01:39:43 -0400

    integration of backend and frontend part 1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import React from "react";
+//import Modal from "./components/Modal";
+import axios from "axios";
 
 function App() {
+  var lst = [1, 2, 3]
+
+  const refreshList = () => {
+    axios
+      .get("http://localhost:8000/api/todos")
+      .then(res => this.setState({lst : res.data}))
+      .catch(err => console.log(err));
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
     </div>
   );
 }
