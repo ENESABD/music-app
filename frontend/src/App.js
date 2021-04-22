b1f7af993ef6cc7b03b017be60c9d2b69b602d4e
ENESABD <72234612+ENESABD@users.noreply.github.com>
2021-04-22 15:41:49 -0400

    Integration of Backend and Frontend | Minor changes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -17,7 +17,7 @@ function App() {
   return (
     <div className="App">
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit {{lst}} and save to reload.
         </p>
     </div>
   );
