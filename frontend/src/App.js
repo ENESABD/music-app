71780a6f945663c3098fb1dfb2553d7e993742cf
ENESABD <72234612+ENESABD@users.noreply.github.com>
2021-04-24 02:31:29 -0400

    Conflict Solving

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,16 +9,8 @@ function App() {
   const [lst,setList] = useState([]);
 
   useEffect(() => {
-<<<<<<< HEAD
     axios.get("http://localhost:8000/api/users/")
       .then(res => setList(res.data))
-=======
-    axios.get("http://localhost:8000/api/ratings/")
-      .then(res => {
-          console.log(res.data);
-          setList(res.data)
-      })
->>>>>>> f1c5922a2e4476e176a3eb9e39e00904a5700c51
       .catch(err => console.log(err)); 
   });
 
@@ -27,13 +19,8 @@ function App() {
   const newItems = list.map((value) => value.username);
 
   return (
-<<<<<<< HEAD
     <div>
         {newItems}
-=======
-    <div className="App">
-        {lst}
->>>>>>> f1c5922a2e4476e176a3eb9e39e00904a5700c51
     </div>
   );
 }
